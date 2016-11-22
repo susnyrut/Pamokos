5b52451412a44be9035514cd4f836a0fb4dac7e2
Kestas <kmalin@gmail.com>
2016-11-22 09:10:03 +0200

    Added a test

---
+++ 7_pamoka_11_22/ChatClient/IntegrationTests/Extentions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace IntegrationTests
+{
+    public class Extentions
+    {
+        public static void AssertThrows(Action a)
+        {
+            try
+            {
+                a();
+                Assert.Fail("It should have thrown an exception.");
+            }
+            catch
+            {
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
---
+++ 7_pamoka_11_22/ChatClient/IntegrationTests/TestChatIntegration.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Chat;
+
+namespace IntegrationTests
+{
+    [TestClass]
+    public class TestChatIntegration
+    {
+        private ChatClient _client;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _client = new ChatClient();
+        }
+
+        [TestMethod]
+        public void Register_WhenCorrectPasswordIsProvided_RegisterMethodSucceeds()
+        {
+            _client.Register("testA", "testA");
+            _client.Register("testB", "testB");
+        }
+
+        [TestMethod]
+        public void Register_WhenIncorrectPasswordIsProvided_RegisterMethodThrowsAnException()
+        {
+            Extentions.AssertThrows(() => _client.Register("testA", "incorrect password"));
+        }
+
+        [TestMethod]
+        public void SendMessage_WhenCorrectUserProvided_MessageIsSent()
+        {
+            _client.Register("testA", "testA");
+
+            _client.SendMessage("testB", "This is my message!");
+
+            _client.Register("testB", "testB");
+            var messages = _client.GetAllMessages(1);
+            Assert.AreEqual(1, messages.Count);
+            Assert.AreEqual("testA", messages[0].From);
+            Assert.AreEqual("This is my message!", messages[0].Text);
+        }
+
+        [TestMethod]
+        public void SendMessage_LithuanianLettersAreSupported()
+        {
+            _client.Register("testA", "testA");
+            _client.SendMessage("testB", "ĄČĘĖĮŠŲŪŽąčęėįšųūž");
+
+            _client.Register("testB", "testB");
+            var messages = _client.GetAllMessages(1);
+            Assert.AreEqual(1, messages.Count);
+            Assert.AreEqual("testA", messages[0].From);
+            Assert.AreEqual("ĄČĘĖĮŠŲŪŽąčęėįšųūž", messages[0].Text);
+        }
+    }
+}
