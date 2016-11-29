77869a72bdcfab36ae488b6df60a933f24255a72
Kestutis Malinauskas <kestutis.malinauskas@visma.com>
2016-11-29 14:39:03 +0200

    Pataisytas aprašymas

--- 8_pamoka_11_29/SyntaxTest/Exercises/Exercise02.cs
+++ 8_pamoka_11_29/SyntaxTest/Exercises/Exercise02.cs
@@ -5,6 +5,8 @@
         /*
           Parašykite viešą eilutės tipo (string) propertį (savybę, property), kurio pavadinimas yra MyName. Šis propertis turi turėti viešą geterį ir viešą seterį.
           Properčių vardai yra rašomi iš didžiosios raidės. Toks rašymo būdas vadinamas "Pascal Case".
+          Properčiai C# kalboje yra traktuojami kaip metodai. Vienas propertis atstoja du metodus kurie yra vadinami geteriu (getter) ir seteriu (setter). 
+          Geteris grąžina reikšmę, seteris ją įrašo. Jei padarysite klaidą aprašant propertį, tai klaida bus vaizduojama kaip neteisingas metodas.
         */
     }
 }
--- 8_pamoka_11_29/SyntaxTest/Exercises/Exercise03.cs
+++ 8_pamoka_11_29/SyntaxTest/Exercises/Exercise03.cs
@@ -4,6 +4,8 @@
     {
         /*
           Parašykite viešą eilutės tipo (string) propertį (savybę, property), kurio pavadinimas yra MyName. Šis propertis turi turėti viešą geterį ir privatų seterį.
+          Properčiai C# kalboje yra traktuojami kaip metodai. Vienas propertis atstoja du metodus kurie yra vadinami geteriu (getter) ir seteriu (setter).
+          Geteris grąžina reikšmę, seteris ją įrašo. Jei padarysite klaidą aprašant propertį, tai klaida bus vaizduojama kaip neteisingas metodas.
 
           Parašykite viešą konstruktorių, kuris primtų vieną string tipo parametrą, kurio pavadinimas yra myName.
         */
